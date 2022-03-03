e49ed938e8a0026beaa1c190a6ca7d867600bcd5
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-03 18:08:08 +0100

    Create initial theme colors

--- components/Theme.ts
+++ components/Theme.ts
@@ -1,13 +1,13 @@
 export const lightTheme = {
-  text: '#000000',
-  link: '#004400',
-  background: '#ffffff',
+  text: 'rgb(68, 68, 68)',
+  link: 'rgb(162, 84, 60)',
+  background: 'rgb(255, 255, 255)',
 }
 
 export const darkTheme = {
-  text: '#ffffff',
-  link: '#00ff00',
-  background: '#000000',
+  text: 'rgb(229, 231, 235)',
+  link: 'rgb(60, 162, 162)',
+  background: 'rgb(16, 12, 8)',
 }
 
 export type ThemeType = typeof lightTheme;
