c015b1d2ac2150eacf9e17dcfd6deddeb84077cd
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-03 15:39:52 +0100

    Add springIn effect to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import PageSectionContainer from '../components/PageSectionContainer';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Christopher Ehrlich</title>
-        <meta name="description" content="Full-Stack Developer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <p>home page</p>
-    </div>
-  )
-}
+    <PageSectionContainer>
+      <>
+        <Head>
+          <title>Christopher Ehrlich</title>
+          <meta name='description' content='Full-Stack Developer' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <p>home page</p>
+      </>
+    </PageSectionContainer>
+  );
+};
 
-export default Home
+export default Home;
