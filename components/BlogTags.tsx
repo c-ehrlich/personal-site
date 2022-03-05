f05f11f821bc28015742e21b20358787b8a519db
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-05 19:02:56 +0100

    Tag Styling

--- components/BlogTags.tsx
+++ components/BlogTags.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 
 const BlogTags = ({ tags, resource }: { tags: string[]; resource: string }) => {
   return (
-    <>
+    <StyledBlogTags>
       {tags
         .map<React.ReactNode>((tag) => (
           <Link key={tag} href={`/${resource}/tag/${tag}`}>
-            <a>#{tag}</a>
+            <a>{tag.toUpperCase()}</a>
           </Link>
         ))
         .reduce((prev, curr) => [prev, ' ', curr])}
-    </>
+    </StyledBlogTags>
   );
 };
+
+const StyledBlogTags = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 8px 16px;
+`;
 
 export default BlogTags;
--- data/project/project1.md
+++ data/project/project1.md
@@ -5,6 +5,12 @@ publishedDate: 2021/02/14
 tags:
   - React
   - Zustand
+  - Another Tag
+  - And one more tag
+  - Material UI
+  - Express
+  - Django
+  - Laravel
 image: /img/project/project1.jpg
 github: https://github.com/c-ehrlich/calculator
 deployed: https://c-ehrlich.github.io/calculator
