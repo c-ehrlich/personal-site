9005c620523c4362b7554538b6051176acbf6b6e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-06 17:50:39 +0100

    Add basic intro to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import PageSectionContainer from '../components/PageSectionContainer';
 
 const Home: NextPage = () => {
@@ -11,7 +12,10 @@ const Home: NextPage = () => {
           <meta name='description' content='Full-Stack Developer' />
           <link rel='icon' href='/favicon.ico' />
         </Head>
-        <p>home page</p>
+        <h1>A quick intro</h1>
+        <p>After Graduating from Central St Martins I worked for designers such as <Link href='https://aitorthroup.com/'><a>Aitor Throup</a></Link> and <Link href='https://www.janjanvanessche.com/'>Jan-Jan van Essche</Link>, and then became a lecturer at the <Link href='https://www.ufg.at/'>University of Art and Design in Linz</Link>.</p>
+        <p>During the Coronavirus pandemic I rediscovered my childhood interest in programming, and started teaching myself Full Stack development.</p>
+        <p>My goal is to work for a mission-driven company to develop real, useful products.</p>
       </>
     </PageSectionContainer>
   );
