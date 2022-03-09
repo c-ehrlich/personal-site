f4fb4b0ea26940cbc173a9d3104b79cef65da132
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-09 12:36:27 +0100

    Switch to static site generation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // flag for enable Styled Components in the Rust compiler
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
   reactStrictMode: true,
 }
 
