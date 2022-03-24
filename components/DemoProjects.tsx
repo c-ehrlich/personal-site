4a0ef2ec8375f31d2b271d8c1696ee597c75a035
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-24 14:00:17 +0100

    Add key to demoProjects

--- components/DemoProjects.tsx
+++ components/DemoProjects.tsx
@@ -12,15 +12,15 @@ const Projects: FC<Props> = ({ demoProjects }) => {
   return (
     <div>
       {demoProjects.map((project) => (
-        <>
-          <ProjectText key={project.frontMatter.title}>
+        <div key={project.frontMatter.title}>
+          <ProjectText >
             <Link passHref href={project.frontMatter.github as string}>
               <ProjectTitle>{project.frontMatter.title}</ProjectTitle>
             </Link>{' '}
             - {project.frontMatter.description}
           </ProjectText>
           <BlogTags tags={project.frontMatter.tags} resource='projects' />
-        </>
+        </div>
       ))}
     </div>
   );
