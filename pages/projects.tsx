b64aa9d272546f487b61cbcc510e35b970fc1668
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-04 19:34:44 +0200

    Add fashion design portfolio

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import Projects from '../components/Projects';
 import DemoProjects from '../components/DemoProjects';
 import { getAllPostsWithFrontMatter } from '../lib/blogUtils';
 import { BlogPostsListItem } from '../types';
+import Link from 'next/link';
 
 interface Props {
   projects: BlogPostsListItem[];
@@ -17,16 +18,22 @@ const projects = (props: Props) => {
         Most of these are hosted on free Heroku instances so please allow them a
         second to spin up 😃
       </p>
+      <p>
+        Additionally you can see my{' '}
+        <Link href='/pdf/ce-portfolio-2020.pdf'>
+          <a>Fashion Design Portfolio</a>
+        </Link>{' '}
+        (pdf, about 5mb).
+      </p>
       <Projects projects={props.projects} />
       <h1 style={{ marginTop: '64px' }}>
         ⚙️ ...and here are some bits and bobs.
       </h1>
       <p>
         I&apos;m always interested in trying new technologies and expanding my
-        understanding of the ones I am already using. These aren&apos;t
-        quite full projects, but a collection of demo projects, tests and
-        experiments. Most have READMEs, some have live demos, all have the
-        source code 😃
+        understanding of the ones I am already using. These aren&apos;t quite
+        full projects, but a collection of demo projects, tests and experiments.
+        Most have READMEs, some have live demos, all have the source code 😃
       </p>
       <DemoProjects demoProjects={props.demoProjects} />
     </PageSectionContainer>
