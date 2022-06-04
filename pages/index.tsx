778f49ad31eea3158ba04371e1ea38166f7778f5
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-06-04 19:36:41 +0200

    Add index copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,21 +14,43 @@ const Home: NextPage = () => {
           <link rel='icon' href='/favicon.ico' />
         </Head>
         <h1>A quick intro</h1>
-        <p>After Graduating from Central St Martins I worked for designers such as <Link href='https://aitorthroup.com/'><a>Aitor Throup</a></Link> and <Link href='https://www.janjanvanessche.com/'>Jan-Jan van Essche</Link>, and then became a lecturer at the <Link href='https://www.ufg.at/'>University of Art and Design in Linz</Link>.</p>
-        <p>During the Coronavirus pandemic I rediscovered my childhood interest in programming, and started teaching myself Full Stack development.</p>
-        <p>My goal is to work for a mission-driven company to develop real, useful products.</p>
+        <p>
+          After Graduating from Central St Martins I worked for designers such
+          as{' '}
+          <Link href='https://aitorthroup.com/'>
+            <a>Aitor Throup</a>
+          </Link>{' '}
+          and{' '}
+          <Link href='https://www.janjanvanessche.com/'>
+            Jan-Jan van Essche
+          </Link>
+          , and then became a lecturer at the{' '}
+          <Link href='https://www.ufg.at/'>
+            University of Art and Design in Linz
+          </Link>
+          .
+        </p>
+        <p>
+          During the Coronavirus pandemic I rediscovered my childhood interest
+          in programming, and started teaching myself Full Stack development.
+        </p>
+        <p>
+          My goal is to combine my skills as a <strong>designer</strong> and{' '}
+          <strong>developer</strong> and work for a mission-driven company to
+          develop real, useful products.
+        </p>
       </>
     </PageSectionContainer>
   );
 };
 
 /**
- * Create the RSS feed on build 
+ * Create the RSS feed on build
  */
 export const getStaticProps = async () => {
   await generateRssFeed();
 
   return { props: {} };
-}
+};
 
 export default Home;
