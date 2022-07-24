d55a3cee5d33a41297b3f5e9ff6baa799a5bb4b1
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-24 17:31:12 +0200

    feat: Add Discord to contacts

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,14 +8,23 @@ const contact: NextPage = () => {
       <h1>👋 You can contact me at:</h1>
       <ul>
         <li>
-          <Link href='mailto:ehrlich.christopher@gmail.com'><a>Email</a></Link>
+          <Link href='mailto:ehrlich.christopher@gmail.com'>
+            <a>Email</a>
+          </Link>
         </li>
         <li>
-          <Link href='https://github.com/c-ehrlich'><a>GitHub</a></Link>
+          <Link href='https://github.com/c-ehrlich'>
+            <a>GitHub</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='https://discordapp.com/channels/@me/190543707592720384'>
+            Discord (cje#1138)
+          </Link>
         </li>
       </ul>
     </PageSectionContainer>
-  )
-}
+  );
+};
 
-export default contact
+export default contact;
