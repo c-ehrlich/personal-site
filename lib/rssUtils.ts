ec000bb505b2f09df307518a2dda52295f551ac9
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-11-03 19:49:53 +0100

    update anket details

--- lib/rssUtils.ts
+++ lib/rssUtils.ts
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import { Feed } from 'feed';
-import { RSSBlogPost } from '../types';
-import { marked } from 'marked';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { Feed } from "feed";
+import { RSSBlogPost } from "../types";
+import { marked } from "marked";
 
-const prism = require('prismjs');
-const loadLanguages = require('prismjs/components/');
+const prism = require("prismjs");
+const loadLanguages = require("prismjs/components/");
 
-import rssFavicon from '../public/favicon.ico';
+import rssFavicon from "../public/favicon.ico";
 
 // https://sreetamdas.com/blog/rss-for-nextjs
 
@@ -16,13 +16,13 @@ const getFilesRoot = process.cwd();
 const publishRoot = process.env.SITE_URL as string;
 
 export async function getBlogPostsForRss(): Promise<RSSBlogPost[]> {
-  const files = fs.readdirSync(path.join(getFilesRoot, 'data', 'blog'));
+  const files = fs.readdirSync(path.join(getFilesRoot, "data", "blog"));
 
   // @ts-ignore FIXME do this in a cleaner way
   return files.reduce((allPosts, postSlug) => {
     const source = fs.readFileSync(
-      path.join(getFilesRoot, 'data', 'blog', postSlug),
-      'utf8'
+      path.join(getFilesRoot, "data", "blog", postSlug),
+      "utf8"
     );
     const { data, content } = matter(source);
 
@@ -31,7 +31,7 @@ export async function getBlogPostsForRss(): Promise<RSSBlogPost[]> {
         source: source,
         frontMatter: data,
         content: content,
-        slug: postSlug.replace('.md', ''),
+        slug: postSlug.replace(".md", ""),
       },
       ...allPosts,
     ];
@@ -42,22 +42,22 @@ export async function generateRssFeed() {
   const posts = await getBlogPostsForRss();
   const date = new Date();
   const author = {
-    name: 'Christopher Ehrlich',
-    email: 'ehrlich.christopher@gmail.com',
+    name: "Christopher Ehrlich",
+    email: "ehrlich.christopher@gmail.com",
     link: `${publishRoot}/blog`,
   };
 
   const feed = new Feed({
     title: "Christopher Ehrlich's Blog",
     description:
-      'Thoughts about Programming, Education, Design, and other things',
+      "Thoughts about Programming, Education, Design, and other things",
     id: `${publishRoot}/blog`,
     link: `${publishRoot}/blog`,
-    image: '/android-chrome-192x192.png',
-    favicon: '/favicon.ico',
+    image: "/android-chrome-192x192.png",
+    favicon: "/favicon.ico",
     copyright: `All rights reserved ${date.getFullYear()}, Christopher Ehrlich`,
     updated: date,
-    generator: 'Feed for Node.js',
+    generator: "Feed for Node.js",
     feedLinks: {
       rss2: `${publishRoot}/rss/feed.xml`,
       json: `${publishRoot}/rss/feed.json`,
@@ -87,8 +87,8 @@ export async function generateRssFeed() {
     });
   });
 
-  fs.mkdirSync('./public/rss', { recursive: true });
-  fs.writeFileSync('./public/rss/feed.xml', feed.rss2());
-  fs.writeFileSync('./public/rss/atom.xml', feed.atom1());
-  fs.writeFileSync('./public/rss/feed.json', feed.json1());
+  fs.mkdirSync("./public/rss", { recursive: true });
+  fs.writeFileSync("./public/rss/feed.xml", feed.rss2());
+  fs.writeFileSync("./public/rss/atom.xml", feed.atom1());
+  fs.writeFileSync("./public/rss/feed.json", feed.json1());
 }
